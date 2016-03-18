7780952bde56991d308ba4c6fedab186635360ca
kingbryan <bpking15@gmail.com>
2016-03-18 09:01:20 -0400

    Gulp compiles all scss files to master and converts to css.

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,13 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 
-gulp.task('styles', function() {
-    gulp.src('sass/**/*.scss')
-        .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('./css/'))
+gulp.task('sass', function() {
+  return gulp.src('sass/master.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('css'))
 });
 
 //Watch task
 gulp.task('default',function() {
-    gulp.watch('sass/**/*.scss',['styles']);
+    gulp.watch('sass/master.scss',['sass']);
 });
